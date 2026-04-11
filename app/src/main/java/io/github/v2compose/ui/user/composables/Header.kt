fe276a6af9b3b781be27d17e08ef41edf90ca0c6
cooaer <cooaer@gmail.com>
2026-04-11 16:18:06 +0800

    Refactor: Set transparent background for User Header TopAppBar

--- app/src/main/java/io/github/v2compose/ui/user/composables/Header.kt
+++ app/src/main/java/io/github/v2compose/ui/user/composables/Header.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
@@ -96,7 +98,11 @@ fun UserToolbar(
                 Icon(Icons.Rounded.Share, contentDescription = "share")
             }
         },
-        scrollBehavior = scrollBehavior
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent,
+            scrolledContainerColor = Color.Transparent
+        )
     )
 }
 
